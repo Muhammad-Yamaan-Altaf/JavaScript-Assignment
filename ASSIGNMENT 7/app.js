e7659c8e7c169286142a01bcb584523344311b76
Muhammad Yamaan Altaf <139477242+Muhammad-Yamaan-Altaf@users.noreply.github.com>
2023-09-07 13:01:33 +0500

    Add files via upload

---
+++ ASSIGNMENT 7/app.js
@@ -0,0 +1,17 @@
+                    //  *ASSIGNMENT1 
+                    //?Q1
+alert("ASSALAM-O-ALAIKUM");
+                    //?Q2
+var Message = "Error! Please enter a valid password";
+alert(Message);
+                    //?Q3
+var Message = "Welcome to JS Land...\n Happy Coding!";
+alert(Message);
+                    //?Q4
+var Message = "Welcome to JS Land...";
+alert(Message);
+var Message = "Happy Coding!\n Prevent this page from creating additional dialogues";
+alert(Message); 
+                    //?Q5
+ console.log("Hello... I can run JS through my web browser's console");
+                   
---
+++ ASSIGNMENT 7/app2.js
@@ -0,0 +1,37 @@
+                    // *ASSIGNMENT2
+                    // ?Q1
+                    var username;
+                    // ?Q2
+var myNamme = "MUhammad Yamaan Altaf";
+                    // ?Q3
+var message;
+message = "Hello World";
+alert(message);
+                    // ?Q4
+var studentName = "Jhone Doe";
+alert(studentName);
+var age = "15 years old";
+alert(age);        
+var courseName = "Certified Mobile Application Development";
+alert(courseName);
+                    // ?Q5
+var message;
+message = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(message);                    
+                    // ?Q6
+var email = "yammanaltaf@gmail.com";
+var message = "My email address is ";
+alert (message+email);
+                    // ?Q7
+var book, message;
+book = "A smarter way to learn JavaScript";
+message = "I am trying to learn from the Book "
+alert(message+book);     
+                    // ?Q8 
+var paragraph = document.createElement("p");
+paragraph.textContent = "Yah! I can write HTML content through JavaScript.";
+document.body.appendChild(paragraph);                   
+                    // ?Q9
+var myString = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(myString);
+document.write(myString);
